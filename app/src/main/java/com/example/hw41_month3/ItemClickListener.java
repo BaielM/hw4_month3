6cef6a8c2adc4f40381a748ee23cc99bec3f6531
BaielM <baiemakhmudov@gmail.com>
2023-08-07 23:20:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw41_month3/ItemClickListener.java
@@ -0,0 +1,6 @@
+package com.example.hw41_month3;
+
+public interface ItemClickListener {
+
+    void onItemClick(MovieModel movieModel);
+}
---
+++ app/src/main/java/com/example/hw41_month3/MovieModel.java
@@ -0,0 +1,15 @@
+package com.example.hw41_month3;
+
+public class MovieModel {
+    int image;
+    String title;
+    String description;
+    String rating;
+
+    public MovieModel(int image, String title, String description, String rating) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+}
